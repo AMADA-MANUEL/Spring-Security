98d126fe56ebce5d60c3bb178ea78fd8c69af409
ammanuejhb024 <manuelamada707@gmail.com>
2026-03-08 06:53:54 +0200

    created the spring security successfully

---
+++ spring-security-demo/src/main/java/com/example/spring_security_demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.spring_security_demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+    @GetMapping
+    public String getDemo(){
+        return("<h1>Spring Security Test Demo Successfully </h1>");
+    }
+
+}
