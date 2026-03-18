4f6c3062d596589a5ae0de485369c404176f004c
ammanuejhb024 <manuelamada707@gmail.com>
2026-03-18 22:02:19 +0200

    Initial commit

--- spring-security-demo/src/main/java/com/example/spring_security_demo/controller/DemoController.java
+++ spring-security-demo/src/main/java/com/example/spring_security_demo/controller/DemoController.java
@@ -21,7 +21,7 @@
 
 
 
-package com.example.spring_security_demo;
+package com.example.spring_security_demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
